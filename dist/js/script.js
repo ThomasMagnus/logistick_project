f60ca6908f17d2f40da104a93a579454b711707f
Eldar <emmagomedov_95@inbox.ru>
2020-05-17 22:28:43 +0300

    Закончил адаптив на 991px

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,8 @@
 const mainHeaderBurger = document.querySelector('.main-header__burger'),
 	burger = document.querySelectorAll('.burger'),
-	mainHeaderList = document.querySelector('.main-header__list');
+	mainHeaderList = document.querySelector('.main-header__list'),
+	mainHeaderItem = document.querySelectorAll('.main-header__item'),
+	up = document.getElementById('up');
 
 mainHeaderBurger.addEventListener('click', () => {
 	burger.forEach(item => {
@@ -9,3 +11,23 @@ mainHeaderBurger.addEventListener('click', () => {
 	mainHeaderList.classList.toggle('main-header__list-left');
 	mainHeaderList.classList.toggle('active_left');
 })
+
+mainHeaderItem.forEach((item) => {
+	item.addEventListener('click', () => {
+		mainHeaderList.classList.toggle('main-header__list-left');
+		mainHeaderList.classList.toggle('active_left');
+		burger.forEach(item => {
+			item.classList.toggle('active');
+		});
+	})
+})
+
+
+
+// $(window).scroll(function(){
+// 	if($(this).scrollTop() > 1000) {
+// 	  $('.up_icon').fadeIn();
+// 	} else {
+// 	  $('.up_icon').fadeOut();
+// 	};
+// });
